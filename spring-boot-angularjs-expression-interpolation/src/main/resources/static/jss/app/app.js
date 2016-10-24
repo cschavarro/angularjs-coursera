eabf7545f4c57a79b054b3f13464afe4f058752f
Steven <Steven@Steven-PC>
2016-10-23 19:10:24 -0500

    Added ng-src and middle level use of ng directives with expression and interpolation

--- spring-boot-angularjs-expression-interpolation/src/main/resources/static/jss/app/app.js
+++ spring-boot-angularjs-expression-interpolation/src/main/resources/static/jss/app/app.js
@@ -1,4 +1,23 @@
 /**
  * 
  */
-()();
+(function() {
+	'use strict';
+	
+	angular.module('MsgApp', [])
+	.controller('MsgController', MsgController);
+	
+	MsgController.$inject = ['$scope'];
+	function MsgController ($scope) {
+		$scope.name = "Cristian";
+		$scope.stateOfBeing = 'hungry';
+		
+		$scope.sayMessage = function() {
+			return 'Cristian likes to eat snacks at night!';
+		}
+		
+		$scope.feedCristian = function() {
+			$scope.stateOfBeing = 'fed';
+		}
+	}
+})();
