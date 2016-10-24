c5b5cd587ad69449ce98f35965e4c062a1ece7d3
Steven <Steven@Steven-PC>
2016-10-23 21:56:38 -0500

    Commit to Module1 Solution

---
+++ spring-boot-angularjs-module1/src/main/java/net/cschavarro/SpringBootAngularjsModule1Application.java
@@ -0,0 +1,12 @@
+package net.cschavarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularjsModule1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularjsModule1Application.class, args);
+	}
+}
