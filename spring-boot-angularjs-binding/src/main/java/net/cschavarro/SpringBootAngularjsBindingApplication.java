a29be0a12e43091d5c677bd2b0296c0498881984
Steven <Steven@Steven-PC>
2016-10-29 21:48:13 -0500

    Added 2-way, 1-way, 1-time binding

---
+++ spring-boot-angularjs-binding/src/main/java/net/cschavarro/SpringBootAngularjsBindingApplication.java
@@ -0,0 +1,12 @@
+package net.cschavarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularjsBindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularjsBindingApplication.class, args);
+	}
+}
